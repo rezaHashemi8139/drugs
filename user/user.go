f358ebcc2e2323b9077670fa6200ae295eacc294
Reza Hashemi <reza.8139@gmail.com>
2024-08-26 00:51:12 +0330

    change validation

--- user/user.go
+++ user/user.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
+var validate = validator.New()
+
 type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID    int    `json:"id" validate:"gte=0"`
+	Name  string `json:"name" validate:"required,min=3,max=32"`
+	Email string `json:"email" validate:"required,email"`
 }
 
 func GetUser(c echo.Context) error {
@@ -34,6 +37,14 @@ func CreateUser(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{
 			"error": "Invalid input",
 		})
+	}
+	if err := validate.Struct(user); err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		errors := make(map[string]string)
+		for _, fieldErr := range validationErrors {
+			errors[fieldErr.Field()] = fieldErr.Tag()
+		}
+		return c.JSON(http.StatusBadRequest, errors)
 	}
 	return c.JSON(http.StatusOK, user)
 }
